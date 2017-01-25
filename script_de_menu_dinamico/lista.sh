54f4810d05d0d78d0567b6ddb7aae8b035ab3b48
andcebrod <acrmontellano@gmail.com>
2017-01-25 12:57:03 +0100

    Add files via upload

---
+++ script_de_menu_dinamico/lista.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+fija1="dialog --menu \"Equipo\" 0 0 0"
+fija2="2>/tmp/eleccion.txt"
+variable=""
+let i=0
+exec<Partidos.txt
+read linea
+while [ "$linea" ]
+do
+	variable="$variable $i \"$linea\" "
+	read linea
+	let i=$i+1
+done
+exec<&-
+
+maqueda="$fija1 $variable $fija2"
+echo $maqueda>/tmp/menu.sh
+chmod +x /tmp/menu.sh
+/tmp/menu.sh
