fa3caacf3ad98f5ac834945509a6aee9187bbfda
andcebrod <acrmontellano@gmail.com>
2017-01-25 12:16:17 +0100

    Create ejercicio.sh

---
+++ ejercicio.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+fija1="dialog --menu\"Equipo\" 0 0 0"
+fija2="2>/tmp/eleccion.txt"
+variable=" "
+read linea
+let i=0
+exec<Partidos.txt
+while [ "$linea" ]
+do
+	variable="$variable $i\"$linea\""
+	read linea
+let i=$i+1
+done
+exec<&-
+maqueda="$fija1 $variable $fija2"
+echo $maqueda>/tmp/menu.sh
+chmod +x /tmp/menu.sh
+/tmp/menu.sh
